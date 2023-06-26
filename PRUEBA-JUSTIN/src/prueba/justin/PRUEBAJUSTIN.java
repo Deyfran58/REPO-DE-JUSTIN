f6df4453cc3b19146516e600ebda592a77a9fbed
albdrag <137427702+albdrag@users.noreply.github.com>
2023-06-25 19:19:26 -0600

    Añadiendo aporte

--- PRUEBA-JUSTIN/src/prueba/justin/PRUEBAJUSTIN.java
+++ PRUEBA-JUSTIN/src/prueba/justin/PRUEBAJUSTIN.java
@@ -2,11 +2,12 @@
 package prueba.justin;
 
 
+
 public class PRUEBAJUSTIN {
 
     
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
+        System.out.println("Esto es la segunda prueba");  
     }
-    
 }
