0b6c890191a8755f27b2b3621da0e2bb4fa51387
ParikTV <137732393+ParikTV@users.noreply.github.com>
2023-06-25 19:43:28 -0600

    Nueva linea

--- PRUEBA-JUSTIN/src/prueba/justin/PRUEBAJUSTIN.java
+++ PRUEBA-JUSTIN/src/prueba/justin/PRUEBAJUSTIN.java
@@ -9,5 +9,6 @@ public class PRUEBAJUSTIN {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
         System.out.println("Esto es la segunda prueba");  
+        System.out.println("Esto es la tercer prueba");  
     }
 }
